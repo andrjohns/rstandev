76cb61d719b223af3bf95d8fe54c7923055c658d
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-09-21 13:30:25 +0300

    standalone functions envs

--- R/stanmodel.R
+++ R/stanmodel.R
@@ -1,5 +1,7 @@
 #' @export
-stanmodel <- R6::R6Class("stanmodel",
+stanmodel <- R6::R6Class(
+  "stanmodel",
+  lock_objects = FALSE,
   public = list(
     hpp_code = NULL,
     model_code = NULL,
@@ -86,7 +88,7 @@ stanmodel <- R6::R6Class("stanmodel",
         dyn.load(libpath, local = TRUE, now = TRUE)
       }
     },
-    expose_functions = function() {
+    expose_functions = function(global = FALSE) {
       model_cpp <- stanc_context$call("stanc", "model", self$model_code, as.array("standalone-functions"))$result
       model_lines <- strsplit(model_cpp, "\n", fixed = TRUE)[[1]]
       funs <- grep("// [[stan::function]]", model_lines, fixed = TRUE)
@@ -99,7 +101,15 @@ stanmodel <- R6::R6Class("stanmodel",
 
       mod_stan_funs <- paste(c(model_lines[1:(funs[1] - 1)], stan_funs), collapse = "\n")
       stan_cpp_locations <- generate_cpp(mod_stan_funs, standalone_funs = TRUE)
-      (source_wrapper(stan_cpp_locations, globalenv()))
+      if (global) {
+        source_wrapper(stan_cpp_locations, globalenv())
+      } else {
+        private$standalone_env <- source_wrapper(stan_cpp_locations, new.env())
+        purrr::walk(stan_funs, function(fun) {
+          fun_name <- decor::parse_cpp_function(fun, is_attribute = TRUE)$name
+          private$add_function(fun_name)
+        })
+      }
     },
     sample = function(data_list, init_list, ...) {
       args <- sample_defaults()
@@ -120,9 +130,13 @@ stanmodel <- R6::R6Class("stanmodel",
   ),
   private = list(
     env = NULL,
+    standalone_env = NULL,
     dynlib_basename = NULL,
     dynlib_ext = NULL,
     dynlib_bytes = NULL,
-    cpp11_export_def = NULL
+    cpp11_export_def = NULL,
+    add_function = function(fun_name) {
+      self[[fun_name]] <- private$standalone_env[[fun_name]]
+    }
   )
 )
